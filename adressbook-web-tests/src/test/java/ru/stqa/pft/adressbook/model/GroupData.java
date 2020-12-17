b902472dcd0f2f580fc952d0f9ceff95600af957
Alena.Badzilouskaya <8256002@gmail.com>
2020-12-17 08:46:51 +0100

    Implemented fluent interface

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/model/GroupData.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/model/GroupData.java
@@ -3,30 +3,38 @@ package ru.stqa.pft.adressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-    private  int id;
-    private final String groupName;
-    private final String groupHeader;
-    private final String groupFooter;
+    private  int id= Integer.MAX_VALUE;
+    private  String groupName;
+    private  String groupHeader;
+    private  String groupFooter;
 
 
-
-    public GroupData(int id, String groupName, String groupHeader, String groupFooter) {
-        this.id = Integer.MAX_VALUE;
-        this.groupName = groupName;
-        this.groupHeader = groupHeader;
-        this.groupFooter = groupFooter;
-    }
-
-    public void setId(int id) {
+    public GroupData withId(int id) {
         this.id = id;
+        return this;
     }
 
-    public GroupData(String groupName, String groupHeader, String groupFooter) {
+    public GroupData withName(String groupName) {
+        this.groupName = groupName;
+        return this;
+    }
+
+    public GroupData withHeader(String groupHeader) {
+        this.groupHeader = groupHeader;
+        return this;
+    }
+
+    public GroupData withFooter(String groupFooter) {
+        this.groupFooter = groupFooter;
+        return this;
+    }
+
+ /*   public GroupData(String groupName, String groupHeader, String groupFooter) {
         this.id = 0;
         this.groupName = groupName;
         this.groupHeader = groupHeader;
         this.groupFooter = groupFooter;
-    }
+    }*/
 
     public int getId() {
         return id;
